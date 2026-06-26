39a590666601e88e60023888028e3ad4f05f392b
Etor2528 <ltornam@gmail.com>
2026-06-26 11:59:51 +0000

    updated certificate links viewable

--- src/data/certificates.ts
+++ src/data/certificates.ts
@@ -6,6 +6,7 @@ export interface Certificate {
   credential?: string;
   description: string;
   status: "Completed" | "In Progress" | "Planned";
+  imageUrl?: string;
 }
 
 export const certificates: Certificate[] = [
@@ -13,13 +14,14 @@ export const certificates: Certificate[] = [
     title: "Cybersecurity Certificate",
     issuer: "ALX Africa",
     year: "2025",
-    image: "https://drive.google.com/file/d/16kAv_slVv8AsFCnjnjgO69HIMMZKy-Ui/view?usp=sharing",
-    credential: "",
+    image: "../../public/cybersecurity.png",
+    imageUrl: "https://drive.google.com/file/d/16kAv_slVv8AsFCnjnjgO69HIMMZKy-Ui/view?usp=sharing",
+        credential: "",
     description:
-      "Completed practical training in cybersecurity fundamentals, networking, Linux, risk management, and security best practices.",
+    "Completed practical training in cybersecurity fundamentals, networking, Linux, risk management, and security best practices.",
     status: "Completed",
   },
-
+  
   {
     title: "Google Cybersecurity Professional Certificate",
     issuer: "Google",
@@ -28,12 +30,13 @@ export const certificates: Certificate[] = [
     description: "Currently pursuing.",
     status: "In Progress",
   },
-
+  
   {
     title: "Professional Foundation Certificate",
     issuer: "ALX Africa",
     year: "2025",
-    image: "https://drive.google.com/file/d/12-GOdnRP5ZYrMXPm9K39mVeN8gCjf3C6/view?usp=sharing",
+    image: "../../public/pf.png",
+    imageUrl: "https://drive.google.com/file/d/12-GOdnRP5ZYrMXPm9K39mVeN8gCjf3C6/view?usp=sharing",
     description: "Completed practical training in professional skills, communication, and workplace readiness.",
     status: "Completed",
   },
